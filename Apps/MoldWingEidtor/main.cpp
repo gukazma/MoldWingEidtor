279261b335c15295ef7853d9560e700b64c5f127
guleixin <18017158729@163.com>
2025-09-01 11:47:38 +0800

    ✨ feat: 修改界面

--- Apps/MoldWingEidtor/main.cpp
+++ Apps/MoldWingEidtor/main.cpp
@@ -365,7 +365,91 @@ static void FramePresent(ImGui_ImplVulkanH_Window *wd)
         check_vk_result(err);
     wd->SemaphoreIndex = (wd->SemaphoreIndex + 1) % wd->SemaphoreCount; // Now we can use the next set of semaphores
 }
+void RenderEditor()
+{
+    // 启用 Docking 支持
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // 可选
 
+    static bool show_demo = false;
+    static bool show_layer_panel = true;
+    static bool show_property_panel = true;
+    static bool show_viewport = true;
+    static bool show_menu_bar = true;
+
+    // 主窗口：填满整个屏幕
+    const ImGuiViewport *main_viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(main_viewport->WorkPos);
+    ImGui::SetNextWindowSize(main_viewport->WorkSize);
+    ImGui::SetNextWindowViewport(main_viewport->ID);
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+
+    ImGui::Begin("Editor", nullptr,
+                 ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+                     ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus);
+
+    ImGui::PopStyleVar(3);
+
+    // 创建 DockSpace
+    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
+
+
+    // 顶部菜单栏
+    if (show_menu_bar && ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            ImGui::MenuItem("Open");
+            ImGui::MenuItem("Save");
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Edit"))
+        {
+            ImGui::MenuItem("Undo");
+            ImGui::MenuItem("Redo");
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+
+    // 左侧面板：层级（例如场景对象列表）
+    if (show_layer_panel)
+    {
+        ImGui::Begin("Layer Panel");
+        ImGui::Text("Scene Hierarchy");
+        ImGui::Separator();
+        if (ImGui::TreeNode("Object 1"))
+        {
+            ImGui::Text("Position: 1.0, 2.0, 3.0");
+            ImGui::TreePop();
+        }
+        if (ImGui::TreeNode("Object 2"))
+        {
+            ImGui::Text("Color: Red");
+            ImGui::TreePop();
+        }
+        ImGui::End();
+    }
+
+    // 中间视口：可渲染 3D 内容或图像
+    if (show_viewport)
+    {
+        ImGui::Begin("Viewport");
+        ImVec2 viewport_size = ImGui::GetContentRegionAvail();
+        ImGui::Text("Viewport - Render your 3D scene or image here");
+        // 在此处绘制纹理或绑定 FBO
+        // ImGui::Image(my_texture, viewport_size);
+        ImGui::End();
+    }
+
+
+    ImGui::End(); // Editor 主窗口
+}
 // Main code
 int main(int, char **)
 {
@@ -506,47 +590,47 @@ int main(int, char **)
         ImGui_ImplVulkan_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        RenderEditor();
+        //// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
+        //// to learn more about Dear ImGui!).
+        //if (show_demo_window)
+        //    ImGui::ShowDemoWindow(&show_demo_window);
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
-        // to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        //{
+        //    static float f = 0.0f;
+        //    static int counter = 0;
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        //    ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+        //    ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+        //    ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+        //    ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+        //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+        //    ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+        //    if (ImGui::Button(
+        //            "Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+        //        counter++;
+        //    ImGui::SameLine();
+        //    ImGui::Text("counter = %d", counter);
 
-            if (ImGui::Button(
-                    "Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+        //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        //    ImGui::End();
+        //}
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window",
-                         &show_another_window); // Pass a pointer to our bool variable (the window will have a closing
-                                                // button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        //// 3. Show another simple window.
+        //if (show_another_window)
+        //{
+        //    ImGui::Begin("Another Window",
+        //                 &show_another_window); // Pass a pointer to our bool variable (the window will have a closing
+        //                                        // button that will clear the bool when clicked)
+        //    ImGui::Text("Hello from another window!");
+        //    if (ImGui::Button("Close Me"))
+        //        show_another_window = false;
+        //    ImGui::End();
+        //}
 
         // Rendering
         ImGui::Render();
